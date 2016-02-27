8b8b8d3996c12bba1466fac32a67eb66787ba6fb
Dmitry Stremous <stremous@list.ru>
2016-02-27 18:51:12 +0300

    add gulp task to inject all neccessary app files and libraries into html

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@
 
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var angularFilesort = require('gulp-angular-filesort');
+var inject = require('gulp-inject');
+var mainBowerFiles = require('main-bower-files');
+var notify = require('gulp-notify');
 
 
 gulp.task('connect', function() {
@@ -11,4 +15,28 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('default', ['connect']);
+// inject all neccessary app files and libraries into html
+gulp.task('inject_sort', function(){
+  return gulp.src('src/index.html')
+  .pipe(inject(
+    gulp.src(mainBowerFiles({
+      paths: {
+        bowerDirectory: 'bower_components',
+        bowerrc: '.bowerrc',
+        bowerJson: 'bower.json'
+      },
+          includeDev: true
+    }), {read: false}), {name: 'bower'}))
+  .pipe(inject(
+    gulp.src(['src/app/*.js','src/app/**/*.js'])
+      .pipe(angularFilesort())
+  ))
+  .pipe(inject(
+    gulp.src(['src/assets/styles/*.css'])
+  ))
+  .pipe(gulp.dest(''))
+  .pipe(connect.reload())
+  .pipe(notify('sort and inject Done! :)'));
+});
+
+gulp.task('default', ['inject_sort', 'connect']);
