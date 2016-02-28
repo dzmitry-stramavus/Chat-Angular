976ce7e4cb7404d09f78ba6a1139b0a5244b9d52
Dmitry Stremous <stremous@list.ru>
2016-02-28 15:59:12 +0300

    add get and send fake methods with http service

--- src/app/chat/chat.controller.js
+++ src/app/chat/chat.controller.js
@@ -5,52 +5,46 @@ angular
   .module('GP-Chat-Angular')
   .controller('chatCtrl', chatCtrl);
 
-  function chatCtrl($timeout) {
+  function chatCtrl($timeout, $interval, messagesService) {
     var chat = this;
+    var reloadTime = 10000;
+    var chatmessages = document.querySelector(".chat-messages");
 
-    chat.status = "";
-
-    chat.messages = [
-      {
-        self: true,
-        user: {
-          name: "Dmitry"
-        },
-        data: "Lorem ipsum dolor sit amet, consectetur ",
-        timestamp: "23:55"
-      },
-      {
-        self: false,
-        user: {
-          name: "Zina"
-        },
-        data: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        timestamp: "00:04"
-      },
-      {
-        self: true,
-        user: {
-          name: "Dmitry"
-        },
-        data: "Lorem ipsum dolor sit amet, ...",
-        timestamp: "00:09"
-      }
-    ];
+    chat.status = '';
+    chat.user = 'Dmitry-S';
+    chat.messages = [];
 
     chat.send = function() {
-      var time = new Date().getTime().toString();
-
-      chat.messages.push({
-        self: true,
-        user: {
-          name: "Dmitry"
-        },
-        data: chat.textbox,
+      var time = new Date().getTime();
+      var message = {
+        user: chat.user,
+        msg: chat.textbox,
         timestamp: time
-      });
+      };
+
+      /*messagesService.send(message);*/
+      chat.messages.push(message);
       chat.status = "sending";
       chat.textbox = "";
       $timeout( function() { chat.status = "" }, 1200 );
+      $timeout( function() { chatmessages.scrollTop = chatmessages.scrollHeight }, 10 );
     };
+
+    messagesService.get().then(success, error);
+    $interval(function(){
+      console.log("get runs controller");
+      messagesService.get().then(success, error);
+    }, reloadTime);
+
+    function success(response) {
+      console.log(response)
+      for (var i = 0, len = response.data.length; i < len; i++) {
+        chat.messages.push(response.data[i]);
+      }
+      $timeout( function() { chatmessages.scrollTop = chatmessages.scrollHeight }, 10 );
+    }
+    function error(error) {
+      console.log("Something goes wrong", error);
+    }
   }
 })();
